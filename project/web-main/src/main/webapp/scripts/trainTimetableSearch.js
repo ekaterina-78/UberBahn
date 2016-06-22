010ea72c23dacae784c4eb780f40b72d197cec9f
Ekaterina Driga <kate.driga@yandex.ru>
2016-06-22 19:05:17 +0300

    validation tuned

--- project/web-main/src/main/webapp/scripts/trainTimetableSearch.js
+++ project/web-main/src/main/webapp/scripts/trainTimetableSearch.js
@@ -19,14 +19,19 @@ $(function () {
 
         if ($("#stationOfDeparture").val() == "null") {
             errorMessageSpan.text("Station of departure is not selected");
-        }
-        else if ($("#stationOfArrival").val() == "null") {
+        } else if ($("#stationOfArrival").val() == "null") {
             errorMessageSpan.text("Station of arrival is not selected");
-        }
-            else if ($("#stationOfDeparture").val() == $("#stationOfArrival").val()){
+        } else if ($("#stationOfDeparture").val() == $("#stationOfArrival").val()){
             errorMessageSpan.text("Stations of departure and arrival must be different");
-        }
-        else {
+        } else if ($("#sinceDate").val().length == 0) {
+            errorMessageSpan.text("Enter since date");
+        }  else if ($("#sinceTime").val().length == 0) {
+            errorMessageSpan.text("Enter since time");
+        }  else if ($("#untilDate").val().length == 0) {
+            errorMessageSpan.text("Enter until date");
+        }  else if ($("#untilTime").val().length == 0) {
+            errorMessageSpan.text("Enter until time");
+        } else {
         window.location.href = "/trainChoose?"
             + "stationOfDeparture=" + stationA + "&"
             + "stationOfArrival=" + stationB + "&"
